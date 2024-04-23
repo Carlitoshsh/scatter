7e405252cee4f89182f7d64992488a85948944d5
Carlos Gómez <caragomezher@unal.edu.co>
2024-04-22 23:56:28 -0500

    Add

---
+++ src/components/Dialog.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+  name: "cagh-dialog",
+  methods: {
+    open() {
+      this.$refs.dialog.show();
+    },
+  },
+};
+</script>
+
+<template>
+  <dialog ref="dialog">
+    <div class="grid gap">
+      <slot></slot>
+      <form method="dialog">
+        <button>Ok</button>
+      </form>
+    </div>
+  </dialog>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script setup>
+import TheWelcome from "../components/TheWelcome.vue";
+import Dialog from "../components/Dialog.vue";
+</script>
+
+<template>
+  <main>
+    <TheWelcome />
+    <button @click="$refs.dialog.open()">Test</button>
+
+    <Dialog ref="dialog">
+      <span>This is my dialog</span>
+    </Dialog>
+  </main>
+</template>
